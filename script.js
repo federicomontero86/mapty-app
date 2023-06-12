9f8e22698cc459fcf7f5eb14548580d1cbea0b5d
Federico Montero <federicomontero86@gmail.com>
2023-06-12 11:06:32 -0300

    Displaying a Map using Leaflet Library

--- script.js
+++ script.js
@@ -12,6 +12,35 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // LEC: Using the Geolocation API
+/*
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/place//@${latitude},${longitude}`
+      );
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+*/
+// LEC: Displaying a Map using Leaflet Library
 
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
@@ -20,6 +49,20 @@ if (navigator.geolocation)
       console.log(
         `https://www.google.com/maps/place//@${latitude},${longitude}`
       );
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
