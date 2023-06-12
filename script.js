cd7e7cc1801ac0c915502591f6e81fdbb77b9cc7
Federico Montero <federicomontero86@gmail.com>
2023-06-12 10:22:47 -0300

    Using the Geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// LEC: Using the Geolocation API
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/place//@${latitude},${longitude}`
+      );
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
