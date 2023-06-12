ad52a4aaf0ce74bdd71a459ef7d671215e02d444
Federico Montero <federicomontero86@gmail.com>
2023-06-12 15:10:47 -0300

    Rendering Workout Input Form

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ if (navigator.geolocation)
       alert('Could not get your position');
     }
   );
-*/
+
 // LEC: Displaying a Map Marker
 
 if (navigator.geolocation)
@@ -112,3 +112,71 @@ if (navigator.geolocation)
       alert('Could not get your position');
     }
   );
+*/
+
+// LEC: Rendering Workout Input Form
+
+let map, mapEvent;
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/place//@${latitude},${longitude}`
+      );
+
+      const coords = [latitude, longitude];
+
+      map = L.map('map').setView(coords, 13);
+      //   console.log(map);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // Handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  // Display marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
